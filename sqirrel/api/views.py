d4f57b5aabba4023141f1e161e65a294b4ee8733
Ole Odendahl <odendahl@rzvn.de>
2020-12-25 18:21:52 +0100

    Changed ApiSchema view to HttpResponse with content type.

--- sqirrel/api/views.py
+++ sqirrel/api/views.py
@@ -1,4 +1,4 @@
-from django.http.response import JsonResponse
+from django.http.response import HttpResponse
 from django.views.generic.base import View
 from graphql.utils import schema_printer
 
@@ -10,4 +10,4 @@ class ApiSchema(View):
     def get(self, request):
         """Returns the current GraphQL Schema definition."""
         schema_json = schema_printer.print_schema(schema)
-        return JsonResponse(schema_json)
+        return HttpResponse(schema_json, content_type='application/json')
